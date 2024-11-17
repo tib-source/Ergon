d0fa2d70e50f89e5dc5239837ab07289fceb69b2
“Tibebe <tibebe.demissie@matillion.com>
2024-11-17 22:27:19 +0000

    feat: use api to fetch data

--- src/main/java/com/tibs/Ergon/ErgonApplication.java
+++ src/main/java/com/tibs/Ergon/ErgonApplication.java
@@ -2,27 +2,10 @@ package com.tibs.Ergon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @SpringBootApplication
 public class ErgonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErgonApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
-	}
-
-
-
 }
