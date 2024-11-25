8c166753e18d0241e537f029948eb048a4576732
“Tibebe <tibebe.demissie@matillion.com>
2024-11-25 23:39:40 +0000

    refactor: moving files

---
+++ src/main/client/src/components/BookingModal/index.tsx
@@ -0,0 +1,14 @@
+import Modal from "../Modal";
+
+interface BookingModalProps extends Modal {
+
+}
+const BookingModal = ({open}) => {
+    return (
+        <Modal>
+
+        </Modal>
+    );
+};
+
+export default BookingModal;
